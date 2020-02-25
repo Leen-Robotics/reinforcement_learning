ed43388f49b9bd41c1c3850bed43c8874216de19
Mohammed b. Terry b. Jack <32687826+mohammedterry@users.noreply.github.com>
2020-02-25 14:53:43 +0000

    Add files via upload

--- leen_rl/__init__.py
+++ leen_rl/__init__.py
@@ -2,6 +2,6 @@
 from .agent_experiment import Agent, RLExperiment
 from .environment_sensor_map import Environment, Sensor, CognitiveMap
 from .state_action import BeliefState, Observation, Action, load_in_action_space
-from .policy_approximator import Policy, PolicyFunction, QFunction, ValueFunction, Approximator, π, φ, V, Q
-from .predefined_policies import RandomExploration, GoalPlanning, ExtremeLearningMachine_PolicyClassifier, MatrixFactorisation_PolicyClassifier
+from .policy_approximator import Policy, PolicyFunction, QFunction, ValueFunction, π, φ, V, Q
+from .predefined_policies import RandomExploration, GoalPlanning
 #=======================
